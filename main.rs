277d722911a4985d5645fe76d2eebe8ea83fb114
Alfonso Sandro Gómez Gómez <79282759+asggWa@users.noreply.github.com>
2023-02-10 15:04:26 +0100

    Pruebas iniciales de ensayo error
    rust y otros

---
+++ main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("This system targing rust.Hello, world!. El sistema comenzará una prueba de compatibilidad que dará opciones si no se cierra sin comenzar. Este sistema está en desarrollo e implementa un shell de comando linux por lo que cualquier incompatibilidad debe tratar de ser retomada ántes de buscar compatibilidad en este sistema. Este repositorio sujeto a cambios espera pronto ser más compatible. Felíz día. Stay Hacking and help deploy...");
+}
